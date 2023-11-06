f3fc0094eb05418d777f68fc206588401a5299e5
Lassi Savolainen <lassi.savolainen@helsinki.fi>
2023-11-06 19:15:03 +0200

    Rikottiin testit tehtävää 10 varten.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -64,3 +64,6 @@ class TestVarasto(unittest.TestCase):
     def test_stringi_toimii_oikein(self):
         tmp = f"{self.varasto}"
         self.assertEqual(tmp, "saldo = 0, vielä tilaa 10")
+
+    def test_rikkinain_testi(self):
+        self.assertEqual(self.varasto.tilavuus, 12)
